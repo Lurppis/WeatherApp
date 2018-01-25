04a10420b029ca1bb0276b1ed92ef64089da6322
Marcin Pilarczyk <marcinpilarczyk@MacBook-Air-Marcin.local>
2018-01-25 15:56:18 +0100

    Added yargs to manage passed arguments, finaly get current location by api

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-var request = require('request');
+//Poland%2C%20Warszawska%2022%20Rumia
+const request = require('request');
+const yargs = require('yargs');
 
-var key = 'AIzaSyADP8-e-GEMMRZ8toYm2bd7j2Wy1axQphc';
+const key = 'AIzaSyADP8-e-GEMMRZ8toYm2bd7j2Wy1axQphc';
+const argv = yargs
+    .option({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch waether for',
+            String: true
+        }
+})
+.help('help','h')
+.argv;
+
+const getEncodedAddress = encodeURIComponent(argv.a)
+
 request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+CA&key=${key}`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?&address=${getEncodedAddress},&key=${key}`,
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Location: lat: ${body.results[0].geometry.location.lat} lng: ${body.results[0].geometry.location.lng} `);
 });

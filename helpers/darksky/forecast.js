05eee23d5ba64b5a0cd004628c2cffc29c56e0db
Marcin Pilarczyk <marcinpilarczyk@MacBook-Air-Marcin.local>
2018-01-26 13:43:48 +0100

    Build callback function in forecast and chain data recived from geocode callback

--- helpers/darksky/forecast.js
+++ helpers/darksky/forecast.js
@@ -1,16 +1,23 @@
 const request = require('request');
+const key = 'a290dcaca533293777074cea84558501';
 
-request({
-    url: 'https://api.darksky.net/forecast/a290dcaca533293777074cea84558501/49.997,20.051',
-    json: true
-}, (error, response, body) => {
-    if (error) {
-        console.log(error);
-    } else if (response.statusCode === 400) {
-        console.log('Unable to fetch weather.');
-    } else if (response.statusCode === 200) {
-        console.log(body.currently.temperature);
-    } else {
-        console.log('Unable to fetch weather.');
-    }
-});
+var getWeather = (latitude, longitude, callback) => {
+    request({
+        url: `https://api.darksky.net/forecast/${key}/${latitude},${longitude}`,
+        json: true
+    }, (error, response, body) => {
+        if (error) {
+            callback('Unable to connect to server');
+        } else if (response.statusCode === 400) {
+            callback('Unable to fetch weather.');
+        } else if (response.statusCode === 200) {
+            callback(undefined, {
+                currently: body.currently
+            })
+        } else {
+            callback('Unable to fetch weather.')
+        }
+    });
+}
+
+module.exports.getWeather = getWeather;

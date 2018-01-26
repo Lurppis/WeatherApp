51d305580fe21331d6e12850666c779e5d4a7a98
Marcin Pilarczyk <marcinpilarczyk@MacBook-Air-Marcin.local>
2018-01-26 16:09:40 +0100

    Add axios to main app rearange folder structure.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // Poland%2C%20Warszawska%2022%20Rumia
 const yargs = require('yargs');
-const geocode = require('./helpers/geocode/geocode');
-const weather = require('./helpers/darksky/forecast');
+const axios = require('axios');
 
 const argv = yargs
     .option({
@@ -15,6 +14,11 @@ const argv = yargs
     .help('help', 'h')
     .argv;
 
+const geocodeKey = 'AIzaSyADP8-e-GEMMRZ8toYm2bd7j2Wy1axQphc';
+const weatherKey = 'a290dcaca533293777074cea84558501';
+const getEncodedAddress = encodeURIComponent(argv.a);
+var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?&address=${getEncodedAddress},&key=${geocodeKey}`;
+
 var convertFToC = (amount) => {
     return ((5 * (amount - 32)) / 9);
 };
@@ -23,18 +27,25 @@ var convertCToF = (amount) => {
     return ((9 + (32 * 5)) / 5);
 }
 
-geocode.geocodeAddress(argv.a, (errorMessage, geocodeAddressResult) => {
-    if (errorMessage) {
-        console.log(errorMessage);
+axios.get(geocodeUrl).then((response) => {
+    if (response.data.status === 'ZERO_RESULTS') {
+        throw new Error('Unable to find that address');
+    }
+    var lat = response.data.results[0].geometry.location.lat;
+    var lng = response.data.results[0].geometry.location.lng;
+    var weatherUrl = `https://api.darksky.net/forecast/${weatherKey}/${lat},${lng}`;
+    console.log(response.data.results[0].formatted_address);
+    return axios.get(weatherUrl);
+}).then((response) => {
+    if (response.data.status === 'ZERO_RESULTS') {
+        throw new Error('Unable to find that address');
+    }
+    console.log(response.data.currently); // Now we can get what we want, or go even deeper for next days.
+    console.log(`It's currently ${convertFToC(response.data.currently.temperature)}C, but it feels like ${convertFToC(response.data.currently.apparentTemperature)}C.`);
+}).catch((error) => {
+    if (error === 'ENOTFOUND') {
+        console.log('Unable to connect to server.');
     } else {
-        console.log(JSON.stringify(geocodeAddressResult, undefined, 2));
-        weather.getWeather(geocodeAddressResult.latitude, geocodeAddressResult.longitude, (errorMessage, weatherResult) => {
-            if (errorMessage) {
-                console.log(errorMessage);
-            } else {
-                console.log(JSON.stringify(weatherResult, undefined, 2));
-                console.log(`It's currently ${convertFToC(weatherResult.currently.temperature)}C, but it feels like ${convertFToC(weatherResult.currently.apparentTemperature)}C.`);
-            }
-        })
+        console.log(error.message);
     }
 });
